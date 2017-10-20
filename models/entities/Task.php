8bac9036b73d2750a33ce75e49fcc159da1ae976
igor_razumovsky <igorrozu@gmail.com>
2017-10-21 01:47:56 +0300

    add task model

---
+++ models/entities/Task.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace app\models\entities;
+
+use Yii;
+
+/**
+ * This is the model class for table "tbl_tasks".
+ *
+ * @property integer $id
+ * @property string $data
+ * @property integer $type
+ * @property integer $status
+ */
+class Task extends \yii\db\ActiveRecord
+{
+    public const TYPE = [
+        'notification' => 1,
+    ];
+
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'tbl_tasks';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['data', 'type'], 'required'],
+            [['data'], 'string'],
+            [['type', 'status'], 'integer'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'data' => 'Data',
+            'type' => 'Type',
+            'status' => 'Status',
+        ];
+    }
+}
