eea6567fc6d3d07d924904ef40fe207e2c7159c2
igor_razumovsky <igorrozu@gmail.com>
2017-10-24 02:00:01 +0300

    fix db file

--- config/db.php
+++ config/db.php
@@ -2,11 +2,11 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'pgsql:host=localhost;dbname=postim',
+    'dsn' => 'pgsql:host=localhost;dbname=postim_db',
     'username' => 'postim_user',
     'password' => 'mc2447382@',
     'charset' => 'utf8',
     'enableSchemaCache'=>true,
-    'schemaCacheDuration' => 300,
+    'schemaCacheDuration' => 1000,
     'schemaCache' => 'cache',
 ];
