7649c5342bb920f3de860f7ca096f74a37ff952c
igor_razumovsky <igorrozu@gmail.com>
2017-10-30 23:46:30 +0300

    fixes in css. add gif for 404 error

--- views/site/404.php
+++ views/site/404.php
@@ -4,7 +4,7 @@ $this->title = 'Ошибка 404 — страница не найдена на P
 <div class="margin-top60"></div>
 <div class="block-content">
     <div class="not-found-block">
-        <h1 class="h1-nf">404</h1>
+        <div class="nf-gif"></div>
         <p class="nf-desc">К сожалению, такой страницы уже нет,<br>или вы ввели неверный адрес</p>
         <p class="nf-rec">Но есть много других, <a href="/">перейдите на главную страницу</a></p>
     </div>
