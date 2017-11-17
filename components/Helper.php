a81806b78a4adda095e4ce412597460d2bdc2edd
jrborisov <jrborisov@yandex.by>
2017-11-17 16:22:48 +0300

    fix price

--- components/Helper.php
+++ components/Helper.php
@@ -96,7 +96,7 @@ class Helper{
 
     public static function getPriceFeature($feature){
 
-        $featurePrice = ['average_bill2','beer_price','price_per_hour_sauna2','price_rolls'];
+        $featurePrice = ['average_bill2','beer_price','price_per_hour_sauna2','price_rolls','price_per_month2','price_room','price_cup_cappuccino','price_evening'];
         if(in_array($feature['features_id'],$featurePrice)){
             return self::mb_ucasefirst($feature['value']) .' BYN';
         }
