9802f222a709eabd036ede663b37c03fbb233828
jrborisov <jrborisov@yandex.by>
2017-11-18 22:21:47 +0300

    add google tack manager

--- views/layouts/mainAuth.php
+++ views/layouts/mainAuth.php
@@ -37,8 +37,23 @@ $countNotif = NotificationUser::getCountNotifications();
     <title><?=$this->title?></title>
     <?php $this->head() ?>
     <?php $this->registerAssetBundle('yii\web\JqueryAsset',yii\web\View::POS_HEAD); ?>
+
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-NJT875T');</script>
+    <!-- End Google Tag Manager -->
+
 </head>
 <body>
+
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NJT875T"
+                  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+
 <?php $this->beginBody() ?>
 
 <?=MainMenuWidget::widget(); ?>
