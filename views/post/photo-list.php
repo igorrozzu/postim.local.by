79a26a5409fe839acf87daac17a086c27ae051ed
jrborisov <jrborisov@yandex.by>
2017-11-29 16:29:49 +0300

    fix index photo

--- views/post/photo-list.php
+++ views/post/photo-list.php
@@ -6,12 +6,13 @@ foreach ($dataProvider->getModels() as $photo):?>
             <a href="<?=Url::to(['user/index', 'id' => $photo->user->id])?>">
                 <img class="avatar-user" src="<?=$photo->user->getPhoto()?>">
             </a>
+            <img class="origin-photo-feed" alt="<?=$photo->post->data??''?>" title="<?=$photo->post->data??''?>" src="<?=$photo->getPhotoPath()?>">
         </div>
     </div>
 <?php endforeach;?>
 <?php if ($hrefNext = $dataProvider->pagination->getLinks()['next'] ?? false): ?>
     <div class="btn-show-more"
          data-selector_replace="#btn-load-post-photos" id="btn-load-post-photos"
-         data-href="<?=$hrefNext?>&loadTime=<?=$loadTime?>&sequence=<?=$sequence?>">
+         data-href="<?=$hrefNext?>&loadTime=<?=$loadTime?>&sequence=<?=$sequence?>&hasTitle=true">
         <p>Показать больше фотографии</p></div>
 <?php endif;?>
