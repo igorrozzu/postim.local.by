ae3d6a816346df01f899ff110b252551b2356af8
igor_razumovsky <igorrozu@gmail.com>
2018-02-02 03:23:55 +0300

    fix migration

--- migrations/m180201_234836_create_tbl_recommended_posts.php
+++ migrations/m180201_234836_create_tbl_recommended_posts.php
@@ -4,7 +4,8 @@ use yii\db\Migration;
 
 class m180201_234836_create_tbl_recommended_posts extends Migration
 {
-    public function safeUp()
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
     {
         $this->execute('create table if not exists tbl_recommended_posts
         (
@@ -14,30 +15,14 @@ class m180201_234836_create_tbl_recommended_posts extends Migration
             key jsonb not null,
             queue json,
             updating_at integer not null
-        );
-        
-        create unique index if not exists tbl_recommended_posts_key_uindex
-            on tbl_recommended_posts (key);'
-        );
-    }
+        );');
 
-    public function safeDown()
-    {
-        $this->dropTable('tbl_recommended_posts');
-    }
-
-    /*
-    // Use up()/down() to run migration code without a transaction.
-    public function up()
-    {
-
+        $this->execute('create unique index if not exists tbl_recommended_posts_key_uindex
+            on tbl_recommended_posts (key);');
     }
 
     public function down()
     {
-        echo "m180201_234836_create_tbl_recommended_posts cannot be reverted.\n";
-
-        return false;
+        $this->dropTable('tbl_recommended_posts');
     }
-    */
 }
