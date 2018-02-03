28e7fd5ddc59c747698ca5cab16aee80afdb27fb
jrborisov <jrborisov@yandex.by>
2018-02-03 14:56:40 +0300

    version erip7

--- services/ipay/EripResponse.php
+++ services/ipay/EripResponse.php
@@ -5,18 +5,23 @@ namespace app\services\ipay;
 class EripResponse extends AEripResponse {
 
 
+    private $templates = [
+        'error' => '<?xml version="1.0" encoding="windows-1251"?><ServiceProvider_Response><Error><ErrorLine>{{message}}</ErrorLine></Error></ServiceProvider_Response>',
+        'info'  => '<?xml version="1.0" encoding="windows-1251"?><ServiceProvider_Response><ServiceInfo><Amount><Debt>{{money}}</Debt><Penalty/></Amount><Info xml:space="preserve"><InfoLine>Номер заказа N {{id}}: </InfoLine><InfoLine>Оплата</InfoLine></Info></ServiceInfo></ServiceProvider_Response>',
+        'storn' => '<?xml version="1.0" encoding="windows-1251"?><ServiceProvider_Response></ServiceProvider_Response>',
+        'transaction_result' => '<?xml version="1.0" encoding="windows-1251"?><ServiceProvider_Response><TransactionResult><Info xml:space="preserve"><InfoLine>{{message}}</InfoLine></Info></TransactionResult></ServiceProvider_Response>',
+        'transaction_start'  => '<?xml version="1.0" encoding="windows-1251"?><ServiceProvider_Response><TransactionStart><ServiceProvider_TrxId>{{id}}</ServiceProvider_TrxId><Info xml:space="preserve"><InfoLine>Заказ {{id}}:</InfoLine><InfoLine>Пополнение счета</InfoLine></Info></TransactionStart></ServiceProvider_Response>'
+    ];
+
 
     public function getResponse(string $type, array $data): string
     {
 
         $response = '';
 
-        $filePath = \Yii::getAlias('@app/services/ipay/templates' . "/{$type}.template");
-
-        if(file_exists($filePath)){
-
-            $template = file_get_contents($filePath);
-
+        if(isset($this->templates[$type]))
+        {
+            $template = $this->templates[$type];
             preg_match_all('/(?<={{)\w+(?=}})/', $template, $attributes);
 
             if(isset($attributes[0])){
@@ -27,12 +32,11 @@ class EripResponse extends AEripResponse {
                     }
                 }
             }
-
             $response = $template;
         }
 
         if($response){
-            return iconv('utf-8','windows-1251', trim($response));
+            return mb_convert_encoding('utf-8', 'windows-1251', trim($response));
         }
 
     }
