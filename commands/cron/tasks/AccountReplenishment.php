3ad80b9a1d867b5ecc2fa645a3afbb4d40ee1a7d
jrborisov <jrborisov@yandex.by>
2018-02-06 09:38:38 +0300

    fix bug erip

--- commands/cron/tasks/AccountReplenishment.php
+++ commands/cron/tasks/AccountReplenishment.php
@@ -20,7 +20,8 @@ class AccountReplenishment extends BaseTask
         $containerDI = new Container();
 
         $this->accountService = $containerDI->get('app\services\account\AccountService');
-        $this->accountService->changeAccount($this->params->user_id, $this->params->changing,
+        $changing = str_replace(",", ".", $this->params->changing);
+        $this->accountService->changeAccount($this->params->user_id, $changing,
             "На счет поступило {$this->params->changing} рублей");
     }
 }
