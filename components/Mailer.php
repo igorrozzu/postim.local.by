1164e0876dc0c3b34a35e71243d2624fc8ddf5d8
jrborisov <jrborisov@yandex.by>
2018-04-11 00:28:28 +0300

    new mail

--- components/Mailer.php
+++ components/Mailer.php
@@ -2,8 +2,40 @@
 
 namespace app\components;
 
+use yii\base\InvalidConfigException;
+
 class Mailer extends \yii\swiftmailer\Mailer
 {
+
+    private $sender;
+    private $transports = [];
+
+    public function __construct( array $config = [] )
+    {
+        parent::__construct( $config );
+        $this->useTransport('info');
+    }
+
+    public function setTransports($transports) {
+        if(!is_array($transports))
+            throw new InvalidConfigException('"' . get_class($this) . '::transport" should be array, "' . gettype($transport) . '" given.');
+        $this->transports = $transports;
+    }
+
+    public function useTransport($name) {
+        if(!array_key_exists($name, $this->transports))
+            throw new InvalidConfigException('"' . get_class($this) . '::useTransport" The requested transport '.$name.' could not be found');
+        $this->setTransport($this->transports[$name]);
+        $this->sender = $this->transports[$name]['username'];
+        return $this;
+    }
+
+    public function compose($view = null, array $params = []) {
+        $message = parent::compose($view, $params);
+        $message->setFrom($this->sender); // By default SMTP Username will be used for sender
+        return $message;
+    }
+
     public function sendMultiple(array $messages)
     {
         if (YII_ENV == 'prod') {
--- config/mailer.php
+++ config/mailer.php
@@ -4,11 +4,21 @@ return [
     'class' => 'app\components\Mailer',
     'useFileTransport' => false,
     'transport' => [
-        'class' => 'Swift_SmtpTransport',
-        'host' => 'smtp.yandex.ru',
-        'username' => 'info@postim.by',
-        'password' => 'mc2447382@',
-        'port' => '465',
-        'encryption' => 'ssl',
+        'info'     => [
+            'class'      => 'Swift_SmtpTransport',
+            'host'       => 'smtp.yandex.ru',
+            'username'   => 'info@postim.by',
+            'password'   => 'mc2447382@',
+            'port'       => '465',
+            'encryption' => 'ssl',
+        ],
+        'feedback' => [
+            'class'      => 'Swift_SmtpTransport',
+            'host'       => 'smtp.yandex.ru',
+            'username'   => 'feedback@postim.by',
+            'password'   => 'vlad1770015@',
+            'port'       => '465',
+            'encryption' => 'ssl',
+        ]
     ],
 ];
